ee11fd14c4f61b43f22f96b0ffb1aa7d3cbc3179
Vlаdislav Sapunоv <155882546+vladislavas88@users.noreply.github.com>
2024-12-23 17:17:10 +0400

    Update Perl_one-liners.pl

--- Perl_one-liners.pl
+++ Perl_one-liners.pl
@@ -2,24 +2,42 @@
 
 # cat ~/in_url >> ~/in_url_archive && cat ~/in_url | perl -nE "s/\./\\\./g; print;" | perl -nE "s/https\:\/\/www\\\./(\^\|\\.\)/g; print;" | perl -nE "s/https\:\/\//\(\^\|\\.\)/g; print;" | perl -nE "s/http\:\/\/www\\./\(\^\|\\.\)/g; print;" | perl -nE "s/http\:\/\//\(\^\|\\.\)/g; print;" | perl -nE "s/\/.*//g; print;" | perl -nE "s/^\s*$//g; print;" >> /etc/squid/acllist/url_allow_whitelist.lst
 
-#============================================================================================================================================
-
-$ cat mail_actual.txt | perl -lanE 'say "$F[2]"'
-
-#============================================================================================================================================
-
-$ cat dc.txt | perl -lanE 'say lc("$_" . ".domain.com")' >> dc_lc.txt
-
-#============================================================================================================================================
-
-$ cat dc_lc.txt | perl -lanE 'say "invoke-Command -ComputerName" . " " . "$_" . " " . "\-ScriptBlock \{ get-hotfix \| where {\$\_\.HotFixID \-like \"KB5048671\"\}\} \| ft \-HideTableHeaders \>> C:\\Temp\\CheckDC.txt"'
+perl -nE "s/\./\\\./g; print;"
+perl -nE "s/https\:\/\/www\\\./(\^\|\\.\)/g; print;"
+perl -nE "s/https\:\/\//\(\^\|\\.\)/g; print;"
+perl -nE "s/http\:\/\/www\\./\(\^\|\\.\)/g; print;"
+perl -nE "s/http\:\/\//\(\^\|\\.\)/g; print;"
+perl -nE "s/\/.*//g; print;"
+perl -nE "s/^\s*$//g; print;"
 
 #============================================================================================================================================
 
-$ cat hotfix_notinstalled1.txt | perl -lanE 'say lc("$_")' >> dc_without_hf.txt
+# $ cat mail_actual.txt | perl -lanE 'say "$F[2]"'
+
+perl -lanE 'say "$F[2]"'
 
 #============================================================================================================================================
 
-$ cat CheckDC.txt | perl -lanE '$_=~ s/^\s+//; if ($_=~ m/^[A-Za-z0-9]/) { chomp($_); say $_}'
+# $ cat dc.txt | perl -lanE 'say lc("$_" . ".domain.com")' >> dc_lc.txt
+
+perl -lanE 'say lc("$_" . ".domain.com")' >> dc_lc.txt
+
+#============================================================================================================================================
+
+# $ cat dc_lc.txt | perl -lanE 'say "invoke-Command -ComputerName" . " " . "$_" . " " . "\-ScriptBlock \{ get-hotfix \| where {\$\_\.HotFixID \-like \"KB5048671\"\}\} \| ft \-HideTableHeaders \>> C:\\Temp\\CheckDC.txt"'
+
+perl -lanE 'say "invoke-Command -ComputerName" . " " . "$_" . " " . "\-ScriptBlock \{ get-hotfix \| where {\$\_\.HotFixID \-like \"KB5048671\"\}\} \| ft \-HideTableHeaders \>> C:\\Temp\\CheckDC.txt"'
+
+#============================================================================================================================================
+
+# $ cat hotfix_notinstalled1.txt | perl -lanE 'say lc("$_")' >> dc_without_hf.txt
+
+perl -lanE 'say lc("$_")'
+
+#============================================================================================================================================
+
+# $ cat CheckDC.txt | perl -lanE '$_=~ s/^\s+//; if ($_=~ m/^[A-Za-z0-9]/) { chomp($_); say $_}'
+
+perl -lanE '$_=~ s/^\s+//; if ($_=~ m/^[A-Za-z0-9]/) { chomp($_); say $_}'
 
 #============================================================================================================================================
