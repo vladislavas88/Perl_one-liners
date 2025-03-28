cd7fda58bd824bf7314d5d5c390fb353789b01dc
Vlаdislav Sapunоv <155882546+vladislavas88@users.noreply.github.com>
2025-03-28 17:53:56 +0400

    Update Perl_one-liners.pl

--- Perl_one-liners.pl
+++ Perl_one-liners.pl
@@ -1,7 +1,7 @@
 #============================================================================================================================================
 
 # cat ~/in_url >> ~/in_url_archive && cat ~/in_url | perl -nE "s/\./\\\./g; print;" | perl -nE "s/https\:\/\/www\\\./(\^\|\\.\)/g; print;" | perl -nE "s/https\:\/\//\(\^\|\\.\)/g; print;" | perl -nE "s/http\:\/\/www\\./\(\^\|\\.\)/g; print;" | perl -nE "s/http\:\/\//\(\^\|\\.\)/g; print;" | perl -nE "s/\/.*//g; print;" | perl -nE "s/^\s*$//g; print;" >> /etc/squid/acllist/url_allow_whitelist.lst
-
+  
 perl -nE "s/\./\\\./g; print;"
 perl -nE "s/https\:\/\/www\\\./(\^\|\\.\)/g; print;"
 perl -nE "s/https\:\/\//\(\^\|\\.\)/g; print;"
