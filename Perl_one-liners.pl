48a5fd12e56edd841985f84150cccb34c51096e4
Vlаdislav Sapunоv <155882546+vladislavas88@users.noreply.github.com>
2025-09-17 23:38:28 +0400

    Update Perl_one-liners.pl

--- Perl_one-liners.pl
+++ Perl_one-liners.pl
@@ -1,5 +1,5 @@
 #============================================================================================================================================
-
+ 
 # cat ~/in_url >> ~/in_url_archive && cat ~/in_url | perl -nE "s/\./\\\./g; print;" | perl -nE "s/https\:\/\/www\\\./(\^\|\\.\)/g; print;" | perl -nE "s/https\:\/\//\(\^\|\\.\)/g; print;" | perl -nE "s/http\:\/\/www\\./\(\^\|\\.\)/g; print;" | perl -nE "s/http\:\/\//\(\^\|\\.\)/g; print;" | perl -nE "s/\/.*//g; print;" | perl -nE "s/^\s*$//g; print;" >> /etc/squid/acllist/url_allow_whitelist.lst
   
 perl -nE "s/\./\\\./g; print;" 
@@ -64,4 +64,5 @@ perl -E 'for($i=10; $i<118; $i++) { say "\(\^\|\\.\)" . "host$i\\.domain\\.com";
 perl -E 'my @boobs = (40, 46, 41, 40, 46, 41); for (@boobs) { print chr($_); } say "- not mine" if "not hairy";'
 
 #============================================================================================================================================
+
 
